9b7743831658f069e4c89394f7c3977b685e2ddb
leahzahn <zahn@chapman.edu>
2021-02-10 13:33:34 -0800

    doing more tutorial stuff

---
+++ Assets/scripts/GameScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+        Debug.Log("clicked");
+    }
+}
